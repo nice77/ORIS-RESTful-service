6afb1314a4f30d95313c03761b6b42fce8ac3a27
nice77 <n.i.y.a.z.19831977@gmail.com>
2024-05-25 20:10:13 +0300

    Added tests

--- src/test/java/org/semester/AuthenticationControllerIntegrationTest.java
+++ src/test/java/org/semester/AuthenticationControllerIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -38,8 +40,26 @@ public class AuthenticationControllerIntegrationTest {
     }
 
     @Test
+    public void testNegativeAuthentication() throws Exception {
+        AuthorizeRequest authorizeRequest = new AuthorizeRequest("1@gmail.com", "1234");
+        ResponseEntity<Object> response = this.authenticationController.authenticate(authorizeRequest);
+        assertThat(response).isNotNull();
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+
+    @Test
     public void testRefresh() throws Exception {
-        RefreshRequest refreshRequest = new RefreshRequest("random_refresh_token_that_does_not_exist");
+        String refreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9BRE1JTiIsInR5cGUiOiJyZWZyZXNoIiwiZW1haWwiOiIxQGdtYWlsLmNvbSIsImlhdCI6MTcxNjY1NjkyNywiZXhwIjoxNzE2NzQzMzI3fQ.FIxliTOZyKcoMrAn5vwaclJaAzGzk58QK90v_wSV7Nk";
+        RefreshRequest refreshRequest = new RefreshRequest(refreshToken);
+        ResponseEntity<Object> response = this.authenticationController.refresh_tokens(refreshRequest);
+        assertThat(response).isNotNull();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testInvalidRefresh() throws Exception {
+        String refreshToken = "refresh_token_that_does_not_exist";
+        RefreshRequest refreshRequest = new RefreshRequest(refreshToken);
         ResponseEntity<Object> response = this.authenticationController.refresh_tokens(refreshRequest);
         assertThat(response).isNotNull();
         assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
